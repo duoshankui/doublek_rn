e50450de3be8ec4ff2278542e32bb2d4e9d78eb5
DoubleK <2578138035@qq.com>
2020-10-27 15:46:14 +0800

    长列表和分组标签用法

--- App.js
+++ App.js
@@ -181,7 +181,7 @@ function Catty(props) {
 function Cafe () {
   return (
     <SafeAreaView>
-    <ScrollView>
+    <ScrollView style={{padding: 10}}>
     <View>
       <Catty name="Maru"/>
       <Catty name="Jellylorum"/>
@@ -196,4 +196,92 @@ function Cafe () {
 //export default App;
 // export default Cat;
 // export default TestApp;
-export default Cafe;
+// export default Cafe;
+
+
+// 长列表 FlatList
+
+import { FlatList } from 'react-native';
+
+const styles1 = StyleSheet.create ({
+  containter: {
+    flex: 1,
+    paddingTop: 66
+  },
+  item: {
+    padding: 10,
+    fontSize: 18.0,
+    height: 40
+  }
+});
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles1.containter}>
+      <FlatList
+        data={[
+          {key: 'Tom'},
+          {key: 'Lily'},
+          {key: 'Jack'},
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles1.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+}
+
+// export default FlatListBasics;
+
+// 分组列表 SectionList
+import { SectionList } from 'react-native'
+
+const sectionStyle = StyleSheet.create ({
+  containter: {
+    flex: 1,
+    paddingTop: 66
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247, 247, 247, 1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18.0,
+    height: 44
+  }
+});
+
+const SectionListBasics = () => {
+  return (
+    <View style={sectionStyle.containter}>
+      <SectionList 
+        sections={[
+          {title: 'D', data:['Devin', 'Dan', 'Dominic']},
+          {title: 'J', data:['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']}
+        ]}
+        renderItem={({item}) => <Text style={sectionStyle.item}>{item}</Text>}
+        renderSectionHeader={({section}) => <Text style={sectionStyle.sectionHeader}>{section.title}</Text>}
+        keyExtractor={(item, index) => index}
+      />
+    </View>
+  );
+}
+
+export default SectionListBasics;
+
+
